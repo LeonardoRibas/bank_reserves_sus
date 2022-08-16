972c41ab1c9b4937213fd84be1ca47f52045a88b
Leo Ribas <leoribasnasc@gmail.com>
2022-08-16 18:30:09 -0300

    Limpa e melhora o batch_run

--- batch_run.py
+++ batch_run.py
@@ -24,209 +24,36 @@ every step of every run.
 """
 
 import itertools
-
 import mesa
 import numpy as np
 import pandas as pd
+from datetime import datetime
+from bank_reserves.model import BankReserves
 
-from bank_reserves.agents import Bank, Person
-
-# Start of datacollector functions
-
-
-def get_num_rich_agents(model):
-    """list of rich agents"""
-
-    rich_agents = [a for a in model.schedule.agents if a.savings > model.rich_threshold]
-    # return number of rich agents
-    return len(rich_agents)
-
-
-def get_num_poor_agents(model):
-    """list of poor agents"""
-
-    poor_agents = [a for a in model.schedule.agents if a.loans > 10]
-    # return number of poor agents
-    return len(poor_agents)
-
-
-def get_num_mid_agents(model):
-    """list of middle class agents"""
-
-    mid_agents = [
-        a
-        for a in model.schedule.agents
-        if a.loans < 10 and a.savings < model.rich_threshold
-    ]
-    # return number of middle class agents
-    return len(mid_agents)
-
-
-def get_total_savings(model):
-    """list of amounts of all agents' savings"""
-
-    agent_savings = [a.savings for a in model.schedule.agents]
-    # return the sum of agents' savings
-    return np.sum(agent_savings)
-
-
-def get_total_wallets(model):
-    """list of amounts of all agents' wallets"""
-
-    agent_wallets = [a.wallet for a in model.schedule.agents]
-    # return the sum of all agents' wallets
-    return np.sum(agent_wallets)
-
-
-def get_total_money(model):
-    """sum of all agents' wallets"""
-
-    wallet_money = get_total_wallets(model)
-    # sum of all agents' savings
-    savings_money = get_total_savings(model)
-    # return sum of agents' wallets and savings for total money
-    return wallet_money + savings_money
-
-
-def get_total_loans(model):
-    """list of amounts of all agents' loans"""
-
-    agent_loans = [a.loans for a in model.schedule.agents]
-    # return sum of all agents' loans
-    return np.sum(agent_loans)
-
-def get_total_debts(model):
-    """list of amounts of all agents' debts"""
-
-    agent_debts = [a.debts for a in model.schedule.agents]
-    # return sum of all agents' debts
-    return int(np.sum(agent_debts))
-
-
-def track_params(model):
-    return (model.init_people, model.rich_threshold, model.reserve_percent, model.suspicion)
-
-
-def track_run(model):
-    return model.uid
-
-
-class BankReservesModel(mesa.Model):
-    # id generator to track run number in batch run data
-    id_gen = itertools.count(1)
-
-    # grid height
-    grid_h = 20
-    # grid width
-    grid_w = 20
-
-    """init parameters "init_people", "rich_threshold", and "reserve_percent"
-       are all set via Slider"""
-
-    def __init__(
-        self,
-        height=grid_h,
-        width=grid_w,
-        init_people=2,
-        rich_threshold=10,
-        reserve_percent=50,
-        suspicion=50
-    ):
-        self.uid = next(self.id_gen)
-        self.height = height
-        self.width = width
-        self.init_people = init_people
-        self.schedule = mesa.time.RandomActivation(self)
-        self.grid = mesa.space.MultiGrid(self.width, self.height, torus=True)
-        # rich_threshold is the amount of savings a person needs to be considered "rich"
-        self.rich_threshold = rich_threshold
-        self.reserve_percent = reserve_percent
-        self.suspicion = suspicion
-        # see datacollector functions above
-        self.datacollector = mesa.DataCollector(
-            model_reporters={
-                "Model Params": track_params,
-                # "Rich": get_num_rich_agents,
-                # "Poor": get_num_poor_agents,
-                # "Middle Class": get_num_mid_agents,
-                "Savings": get_total_savings,
-                "Wallets": get_total_wallets,
-                "Money": get_total_money,
-                "Loans": get_total_loans,
-                "Debts": get_total_debts,
-                "Run": track_run,
-            },
-            agent_reporters={"Wealth": "wealth"},
-        )
-
-        # create a single bank for the model
-        self.bank = Bank(1, self, self.reserve_percent)
-
-        # create people for the model according to number of people set by user
-        for i in range(self.init_people):
-            # set x coordinate as a random number within the width of the grid
-            x = self.random.randrange(self.width)
-            # set y coordinate as a random number within the height of the grid
-            y = self.random.randrange(self.height)
-            p = Person(i, (x, y), self, True, self.bank, self.rich_threshold, self.suspicion)
-            # place the Person object on the grid at coordinates (x, y)
-            self.grid.place_agent(p, (x, y))
-            # add the Person object to the model schedule
-            self.schedule.add(p)
-
-        self.running = True
-
-    def step(self):
-        # collect data
-        self.datacollector.collect(self)
-        # tell all the agents in the model to run their step function
-        self.schedule.step()
-
-    def run_model(self):
-        for i in range(self.run_time):
-            self.step()
-
+max_steps_per_simulation=100
+iterations_per_parameter=10
 
 # parameter lists for each parameter to be tested in batch run
 br_params = {
-    "init_people": 100,
+    "init_people": 200,
     "rich_threshold": 20,
     "reserve_percent": 50,
-    "suspicion": [0, 0.2, 0.4, 0.6, 0.8, 1]
+    "suspicion": np.arange(0, 1, 0.1)
 }
+
 
 if __name__ == "__main__":
     data = mesa.batch_run(
-        BankReservesModel,
+        BankReserves,
         br_params,
+        iterations=iterations_per_parameter,
+        max_steps=max_steps_per_simulation,
     )
     br_df = pd.DataFrame(data)
-    br_df.to_csv("BankReservesModel_Data.csv")
 
-    # The commented out code below is the equivalent code as above, but done
-    # via the legacy BatchRunner class. This is a good example to look at if
-    # you want to migrate your code to use `batch_run()` from `BatchRunner`.
-    # Things to note:
-    # - You have to set "reserve_percent" in br_params to `[5]`, because the
-    #   legacy BatchRunner doesn't auto-detect that it is single-valued.
-    # - The model reporters need to be explicitly specified in the legacy
-    #   BatchRunner
-    """
-    from mesa.batchrunner import BatchRunnerMP
-    br = BatchRunnerMP(
-        BankReservesModel,
-        nr_processes=2,
-        variable_parameters=br_params,
-        iterations=2,
-        max_steps=1000,
-        model_reporters={"Data Collector": lambda m: m.datacollector},
-    )
-    br.run_all()
-    br_df = br.get_model_vars_dataframe()
-    br_step_data = pd.DataFrame()
-    for i in range(len(br_df["Data Collector"])):
-        if isinstance(br_df["Data Collector"][i], DataCollector):
-            i_run_data = br_df["Data Collector"][i].get_model_vars_dataframe()
-            br_step_data = br_step_data.append(i_run_data, ignore_index=True)
-    br_step_data.to_csv("BankReservesModel_Step_Data.csv")
-    """
+    now = str(datetime.now()).replace(":","-")
+    file_name_suffix = (str(iterations_per_parameter)+"iter_"+
+                        str(max_steps_per_simulation)+"steps_"+
+                        now)
+
+    br_df.to_csv("BankReservesModelSus_Data"+file_name_suffix+".csv")
